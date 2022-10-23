785e3229ca381024fda54715888850135973f5ee
Javier Cicchelli <javier@rock-n-code.com>
2022-10-23 12:39:42 +0200

    Added a public "init()" function in the XCConfigService service and make its "read(fromURL:)" and "write(rows: toURL:)" function public.

--- Sources/Actors/XCConfigService.swift
+++ Sources/Actors/XCConfigService.swift
@@ -12,16 +12,22 @@ public actor XCConfigService {
     
     // MARK: Properties
     
-    private let handler: XCConfigFileHandler = XCConfigHandler()
+    private let handler: XCConfigFileHandler
+    
+    // MARK: Initialisers
+    
+    public init() {
+        self.handler = XCConfigHandler()
+    }
     
     // MARK: Functions
     
-    func read(fromURL url: URL) async throws -> [any XCConfigRow] {
+    public func read(fromURL url: URL) async throws -> [any XCConfigRow] {
         try await handler.verify(url: url)
         return try await handler.read(fromURL: url)
     }
     
-    func write(rows: [any XCConfigRow], toURL url: URL) async throws {
+    public func write(rows: [any XCConfigRow], toURL url: URL) async throws {
         try await handler.verify(url: url)
         try await handler.write(rows: rows, toURL: url)
     }
