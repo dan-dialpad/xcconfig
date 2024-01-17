17a2314005106ad0e7cbbd3a25ec6f9bdfde035b
Dan Carlson <63247371+dan-dialpad@users.noreply.github.com>
2024-01-17 15:57:36 -0500

    Update XCConfigRowProperty.swift to have public initializer

--- Sources/Models/XCConfigRowProperty.swift
+++ Sources/Models/XCConfigRowProperty.swift
@@ -9,6 +9,11 @@
 public struct XCConfigRowProperty {
     public let key: String
     public let value: String
+
+    public init(key: String, value: String) {
+        self.key = key
+        self.value = value
+    }
 }
 
 // MARK: - XCConfigRowType
